009b692d9e2e57dbab6acd6c2a1f7aa44be3d50e
Sumon360 <Ahmedsumon221133@gmail.com>
2021-02-01 21:39:42 +0300

    array slice array splice and array join

---
+++ array-split.js
@@ -0,0 +1,15 @@
+// array slice
+const number = [34, 3, 5, 54, 56, 45, 65];
+const result = number.slice(0,2);
+console.log(result, number);
+
+// array splice mane kono kichu array theke remove kore dewa mane koto number theke suru hobe tarpor koto ta porjonto remove korbe
+const number = [34, 3, 5, 54, 56, 45, 65];
+const result = number.splice(0,2);
+console.log(result, number);
+
+
+// array join maneh holo array er sobgula ke ak sathe kora ba kono kichu add kora
+const number = [34, 3, 5, 54, 56, 45, 65];
+const result = number.join(" ");
+console.log(result);
