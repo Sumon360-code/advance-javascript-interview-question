ce7249f4063bb6a117d66ce19cd8baba3a5d42f4
Sumon360 <Ahmedsumon221133@gmail.com>
2021-02-01 19:45:37 +0300

    true vs false and null vs undefined

---
+++ interview-releated-question.js
@@ -0,0 +1,38 @@
+if else true false condition 
+
+false value
+0;
+"";
+NaN
+null;
+undefined
+false
+
+true value 
+"0";
+" ";
+[];
+{};
+true
+" false ";
+
+
+undefined vs null
+
+
+undefined 
+let name;
+console.log(name);
+function name(num1,num2){
+    console.log(num1+num2);
+}
+const nameTotal = name(12, 14);
+result will be undefined because we didn't return anything from the function
+
+function doSomething(x, y){
+
+    console.log(y);
+    
+    }
+    
+    doSomething(32);
