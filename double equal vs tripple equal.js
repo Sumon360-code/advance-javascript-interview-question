9374a80af7453b167fadc489c09de94e410154fc
Sumon360 <Ahmedsumon221133@gmail.com>
2021-02-01 19:55:43 +0300

    double equal vs tripple equal

---
+++ double equal vs tripple equal.js
@@ -0,0 +1,9 @@
+
+== double equal
+just value check korbe
+double equal mane holo jodi variable er vitore soman kono kichu thake jemon number hok ba string thakuk jodi soman hoi // example 2 ar "2" ei duita jodio akta string ar akta number but double equal dile ef else condition e eigula true dekhabe 
+
+
+=== triple equal;
+value plus type sobkichui check korbe
+triple equal maneh holo sobkichu strictly check korbe je soman kina jodi na hoi taile false kore dibe
